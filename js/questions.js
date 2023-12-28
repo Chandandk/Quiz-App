88b21c6e4785e21de110c5e3be95633b16e34aad
Chandan Kumar <c.singh.8425@gmail.com>
2023-12-28 14:49:44 +0530

    committed

---
+++ js/questions.js
@@ -0,0 +1,162 @@
+// creating an array and passing the number, questions, options, and answers
+// MCQ -- pipes & cisterns question and answer
+let pipes = [
+  {
+  
+    numb: 1,
+    question:
+      "Three pipes A, B and C can fill a tank from empty to full in 30 minutes, 20 minutes, and 10 minutes respectively. When the tank is empty, all the three pipes are opened. A, B and C discharge chemical solutions P,Q and R respectively. What is the proportion of the solution R in the liquid in the tank after 3 minutes?",
+    answer: "6/11",
+    options: ["5/11", "6/11", "7/11", "8/11"],
+  },
+  {
+   
+    numb: 2,
+    question:
+      "Pipes A and B can fill a tank in 5 and 6 hours respectively. Pipe C can empty it in 12 hours. If all the three pipes are opened together, then the tank will be filled in",
+    answer: "60/17 hours",
+    options: ["64/17 hours", "30/11 hours", "60/17 hours", "9/2 hours"],
+  },
+  {
+    
+    numb: 3,
+    question:
+      "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in",
+    answer: "14 hours",
+    options: ["13/3 hours", "7 hours", "8 hours", "14 hours"],
+  },
+  {
+  
+    numb: 4,
+    question:
+      "A tank is filled by three pipes with uniform flow. The first two pipes operating simultaneously fill the tank in the same time during which the tank is filled by the third pipe alone. The second pipe fills the tank 5 hours faster than the first pipe and 4 hours slower than the third pipe. The time required by the first pipe is",
+    answer: "15 hours",
+    options: ["6 hours", "10 hours", "15 hours", "30 hours"],
+  },
+  {
+   
+    numb: 5,
+    question:
+      "Two pipes can fill a tank in 20 and 24 minutes respectively and a waste pipe can empty 3 gallons per minute. All the three pipes working together can fill the tank in 15 minutes. The capacity of the tank is",
+    answer: "120 gallons",
+    options: ["60 gallons", "100 gallons", "120 gallons", "180 gallons"],
+  },
+];
+// MCQ -- prob question and answer
+let prob = [
+  {
+    numb: 1,
+    question:
+      "A card is drawn from the set of 52 cards. Find the probability of getting a queen card.",
+    answer: "1/13",
+    options: [" 1/26", " 1/13", "4/53", "4/13"],
+  },
+  {
+     numb: 2,
+    question: "The probability of event equal to zero is called;",
+    answer: " Independent event",
+    options: [
+      "Unsure event",
+      "Sure Event",
+      "Impossible event",
+      "Independent event",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "The sum of the probabilities of all the elementary events of an experiment is",
+    answer: "1",
+    options: [" 0.5", "1", "2", "1.5"],
+  },
+  {
+    numb: 4,
+    question:
+      "If the probability of an event is p, the probability of its complementary event will be",
+    answer: "1 – p",
+    options: ["p – 1", "p", " 1 – p", "1 – 1/p"],
+  },
+  {
+    numb: 5,
+    question:
+      "The probability that a non leap year selected at random will contain 53 Sundays is",
+    answer: " 1/7",
+    options: [" 1/7", " 2/7", " 3/7", " 5/7"],
+  },
+];
+// MCQ -- ages question and answer
+let ages = [
+  {
+    numb: 1,
+    question:
+      "The respective ages of a father and his son are 41 and 16 years. In how many years will the father be twice as old as his son?",
+    answer: "9 years",
+    options: ["19 years", "9 years", "15 years", "10 years"],
+  },
+  {
+    numb: 2,
+    question:
+      "Three years ago the average age of Abhya and Avani was 18 years. With Pari joining them now, the average becomes 22 years. How old is Pari now?",
+    answer: "24 years",
+    options: ["24 years", "26 years", " 23 years", "25 years"],
+  },
+  {
+    numb: 3,
+    question:
+      "The product of the ages of Bill and Jeff is 240. If twice the age of Jeff is more than Bill’s age by 4 years, what is Jeff’sage",
+    answer: "12 years",
+    options: ["10 years", "11 years", "12 years", "13 years"],
+  },
+  {
+    numb: 4,
+    question:
+      " In 10 years, Rupal will be twice as old as Briana was 10 years ago. If Rupal is now 9 years older than Briana, find the present age of Briana.",
+    answer: "39",
+    options: ["39", "27", "45", "26"],
+  },
+  {
+    numb: 5,
+    question:
+      "The difference between the ages of two persons is 8 years. 15 years ago, the elder one was twice as old as the younger one. Then the present age of the elder person is",
+    answer: "31 years",
+    options: ["23 years", "31 years", "34 years", "40 years"],
+  },
+];
+// MCQ -- profit question and answer
+let profit = [
+  {
+    numb: 1,
+    question:
+      "A person sold a stove for Rs. 423 and incurred a loss of 6%. At what price would it be sold so as to earn a profit of 8%?",
+    answer: "Rs. 486",
+    options: [" Rs. 525", "Rs. 500", "Rs. 490", "Rs. 486"],
+  },
+  {
+    numb: 2,
+    question:
+      "A fruit seller buys lemons at 2 for a rupee and sells then at 5 for three rupees. His gain percent is",
+    answer: "20%",
+    options: ["10%", " 15%", " 20%", "25%"],
+  },
+  {
+    numb: 3,
+    question:
+      "A sells a car to B at 10% loss. If B sells it for Rs. 54000 and gains 20%, the cost price of the car for A was",
+    answer: "Rs. 50000",
+    options: ["Rs. 25000", "Rs. 50000", "Rs. 37500", "Rs. 60000"],
+  },
+  {
+    numb: 4,
+    question:
+      "Two bicycles were sold for Rs. 3990 each, gaining 5% on one and losing 5% on the other. The gain or loss percent on the whole transaction is",
+    answer: " 0.25% loss",
+    options: ["Neither gain nor loss", "2.5% gain", "2.5% loss", " 0.25% loss"],
+  },
+  {
+    numb: 5,
+    question:
+      "The ratio of cost price and selling price is 4:5. The profit percent is",
+    answer: "25%",
+    options: ["10%", "20%", "25%", " 30%"],
+  },
+];
